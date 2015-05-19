43f7d7fb785ffa7ecfc3053d59f652ac0367763f
orizens <farhioren@gmail.com>
2015-05-19 13:05:05 +0300

    fixed: updated syntax for jasmine 2, in package.json as well, currently 1 test is failing

--- test/controller.spec.js
+++ test/controller.spec.js
@@ -6,12 +6,12 @@ describe('controller', function () {
     var subject, model, view;
 
     var setUpModel = function (todos) {
-        model.read.andCallFake(function (query, callback) {
+        model.read.and.callFake(function (query, callback) {
             callback = callback || query;
             callback(todos);
         });
 
-        model.getCount.andCallFake(function (callback) {
+        model.getCount.and.callFake(function (callback) {
 
             var todoCounts = {
                 active: todos.filter(function (todo) {
@@ -26,15 +26,15 @@ describe('controller', function () {
             callback(todoCounts);
         });
 
-        model.remove.andCallFake(function (id, callback) {
+        model.remove.and.callFake(function (id, callback) {
             callback();
         });
 
-        model.create.andCallFake(function (title, callback) {
+        model.create.and.callFake(function (title, callback) {
             callback();
         });
 
-        model.update.andCallFake(function (id, updateData, callback) {
+        model.update.and.callFake(function (id, updateData, callback) {
             callback();
         });
     };
@@ -64,7 +64,7 @@ describe('controller', function () {
         }
         document.body.appendChild(div);
     };
-    
+
     beforeEach(function (done) {
         setTimeout(function () {
             setupTemplates();
@@ -235,7 +235,7 @@ describe('controller', function () {
 
             view.render.reset();
             model.read.reset();
-            model.read.andCallFake(function (callback) {
+            model.read.and.callFake(function (callback) {
                 callback([{
                     title: 'a new todo',
                     completed: false
