621cf2eeb52f1051ecafa24fcab33c2432134f53
sefigiga <gigaS3f1nbar>
2015-06-08 21:14:32 +0300

    branch(step): create step branch
    create step 5 code challenge branch

--- test/spec/ItemView.spec.js
+++ test/spec/ItemView.spec.js
@@ -1,29 +1,3 @@
 describe("ItemView - ", function () {
-    var model, view;
-    var desc = "buy 3% milk";
 
-	beforeEach(function() {
-		model = new TodoApp.TodoItem(desc);
-		view = new TodoApp.ItemView(model);
-	});
-
-    it("should render a todo item element", function () {
-        expect(view.$el).toHaveClass('view');
-    });
-
-    it("should have a 'Remove' button", function () {
-
-    });
-
-    it("should mark the item as done when clicked on the input", function () {
-
-    });
-
-    it("should have 'click' to check the item", function () {
-
-    });
-
-    it("should have a 'remove' button with 'click' handler", function () {
-
-    });
 });
--- test/spec/TodoList.spec.js
+++ test/spec/TodoList.spec.js
@@ -1,103 +1,104 @@
-describe("TodoList - ", function() {
-	var list;
-	var url = 'mocks/items.json';
-	var items = [{
-	"description": "buy milk"
-		}, {
-			"description": "pick the kids from school"
-		}, {
-			"description": "clean the car"
-		}, {
-			"description": "have fun"
-		}];
-	var defineAjaxSpy = function() {
-		$.ajax = jasmine.createSpy().and.callFake(function(res){
-			var d = $.Deferred();
-		    d.resolve(items);
-		    return d.promise();
-		});
-	};
+describe("TodoList - ", function () {
+    var list;
+    var url = 'mocks/items.json';
+    var items = [{
+        "description": "buy milk"
+    }, {
+        "description": "pick the kids from school"
+    }, {
+        "description": "clean the car"
+    }, {
+        "description": "have fun"
+    }];
+    var defineAjaxSpy = function () {
+        $.ajax = jasmine.createSpy().and.callFake(function (res) {
+            var d = $.Deferred();
+            d.resolve(items);
+            return d.promise();
+        });
+    };
 
-	beforeEach(function() {
-		list = new TodoApp.TodoList(url);
-	});
+    beforeEach(function () {
+        list = new TodoApp.TodoList(url);
+    });
 
-	it("should have no items", function() {
-		expect(list.items).toBeDefined();
-		expect(list.items.length).toBe(0);
-	});
+    it("should have no items", function () {
+        expect(list.items).toBeDefined();
+        expect(list.items.length).toBe(0);
+    });
 
-	it("should have a url", function() {
-		expect(list.url).toBeDefined();
-		expect(list.url).toBe(url);
-	});
+    it("should have a url", function () {
+        expect(list.url).toBeDefined();
+        expect(list.url).toBe(url);
+    });
 
-	it("should fetch items and populate the items array", function(){
-		defineAjaxSpy();
+    it("should fetch items and populate the items array", function () {
+        defineAjaxSpy();
 
-		list.fetch();
+        list.fetch();
 
-		expect(list.items.length).toBe(items.length);
-	});
+        expect(list.items.length).toBe(items.length);
+    });
 
-	it("should add a new item", function(){
-		var first = items[0];
-		spyOn(list, 'add');
-		list.add(first.description);
-		expect(list.add).toHaveBeenCalledWith(first.description);
-	});
+    it("should add a new item", function () {
+        var first = items[0];
+        spyOn(list, 'add');
+        list.add(first.description);
+        expect(list.add).toHaveBeenCalledWith(first.description);
+    });
 
-	xit("should archive an item", function(){
-		list.archive();
-	});
+    xit("should archive an item", function () {
+        list.archive();
+    });
 
-	it("should call add 4 times", function(){
-		var spyAdd = spyOn(list, 'add');
-		items.forEach(function(item){
-			list.add(item.description);
-		});
-		expect(spyAdd.calls.count()).toEqual(4);
-	});
+    it("should call add 4 times", function () {
+        var spyAdd = spyOn(list, 'add');
+        items.forEach(function (item) {
+            list.add(item.description);
+        });
+        expect(spyAdd.calls.count()).toEqual(4);
+    });
 
-	it("should add the items with 'fetch' without ajax", function(){
-		var addAll = function(){
-			items.forEach(function(item){
-				list.add(item.description);
-			});
-		};
-		var spyAdd = spyOn(list, 'addItems').and.callFake(addAll);
-		list.addItems();
-		expect(list.items.length).toEqual(4);
-	});
+    it("should add the items with 'fetch' without ajax", function () {
+        var addAll = function () {
+            items.forEach(function (item) {
+                list.add(item.description);
+            });
+        };
+        var spyAdd = spyOn(list, 'addItems').and.callFake(addAll);
+        list.addItems();
+        expect(list.items.length).toEqual(4);
+    });
 
-	describe("when a list doesn't have a url", function(){
-		beforeEach(function() {
-			list = new TodoApp.TodoList();
-		});
+    describe("when a list doesn't have a url", function () {
+        beforeEach(function () {
+            list = new TodoApp.TodoList();
+        });
 
-		it("shouldn't have a url set as a property", function(){
-			expect(list.url).toBe();
-		});
+        it("shouldn't have a url set as a property", function () {
+            expect(list.url).toBe();
+        });
 
-		it("should throw an error when trying to fetch items", function(){
-			defineAjaxSpy();
-			expect(list.fetch).toThrow();
-		});
-	});
+        it("should throw an error when trying to fetch items", function () {
+            defineAjaxSpy();
+            expect(list.fetch).toThrow();
+        });
+    });
 
-	describe("Async Specs", function(){
+    describe("Async Specs", function () {
+        // implement an async test!
 
-		beforeEach(function(done){
+        beforeEach(function (done) {
 
-		});
+        });
 
-		it("should wait 1 second before adding 4 items", function(){
+        it("should wait 1 second before adding 4 items", function () {
 
-		});
-	});
+        });
+    });
 
-	afterEach(function(){
-		list.items.length = 0;
-	});
+    afterEach(function () {
+        list.items.length = 0;
+    });
 
 });
